01ebc650c88b7107ebfb4c81b91974b68471745d
MohammedAteeb <mohammedateeb390@gmail.com>
2025-04-30 16:38:33 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -4,6 +4,6 @@ public class hello{
         int a=10;
         int b=20;
         int c=a+b;
-        System.out.println(c);
+        System.out.println("your numbers "+a+" and"+b+ " addition is: "+c);
     }
 }
